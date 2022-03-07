f26880abc8cb3b2959608d30dd571f5a5519d31d
AndrewsVPS <andrewsvpsilva@gmail.com>
2022-03-07 12:55:15 -0300

    Backend StructureNew

---
+++ EvoSystems.Data/Context/EvoContext.cs
@@ -0,0 +1,22 @@
+﻿using EvoSystems.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvoSystems.Data.Context
+{
+    public class EvoContext : DbContext
+    {
+        public EvoContext (DbContextOptions<EvoContext> option)
+            : base(option) { }
+
+        #region "DBSets"
+
+        public DbSet<User> Users { get; set; }
+
+        #endregion
+    }
+}
---
+++ EvoSystems.Data/Mappings/UserMap.cs
@@ -0,0 +1,17 @@
+﻿using EvoSystems.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvoSystems.Data.Mappings
+{
+    public class UserMap : IEntityTypeConfiguration<User>
+    {
+
+
+
+    }
+}
